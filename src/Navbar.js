40dbe087428eaac26345df26fa85e6bbcd880026
jaytanet02 <jaytanet02@gmail.com>
2023-07-29 22:56:56 +0700

    index

--- src/Navbar.js
+++ src/Navbar.js
@@ -2,6 +2,17 @@ import React from 'react'
 import { Link } from 'react-router-dom';
 
 function Navbar() {
+  
+  function checkbody() {
+    document.body.classList.toggle('g-sidenav-show');
+    // หากมีคลาส "bg-gray-100" ใน <body> ให้ลบออก
+    document.body.classList.toggle('bg-gray-100');
+    // หากมีคลาส "g-sidenav-pinned" ใน <body> ให้ลบออก
+    document.body.classList.toggle('g-sidenav-pinned');
+
+
+  };
+
   return (
     <>
     
@@ -9,7 +20,7 @@ function Navbar() {
   <aside class="sidenav bg-white navbar navbar-vertical navbar-expand-xs border-0 border-radius-xl my-3 fixed-start ms-4 " id="sidenav-main">
     <div class="sidenav-header">
       <i class="fas fa-times p-3 cursor-pointer text-secondary opacity-5 position-absolute end-0 top-0 d-none d-xl-none" aria-hidden="true" id="iconSidenav"></i>
-      <a class="navbar-brand m-0" href="http://localhost:3000/dashboard" target="_blank"  rel="noreferrer">
+      <a class="navbar-brand m-0" href="http://localhost:3000/dashboard" target="_blank"  rel="noreferrer" >
         <img src="../assets/img/logo-ct-dark.png" class="navbar-brand-img h-100" alt="main_logo"/>
         <span class="ms-1 font-weight-bold"> ห้องเช่า</span>
       </a>
@@ -18,7 +29,7 @@ function Navbar() {
     <div class="collapse navbar-collapse  w-auto " id="sidenav-collapse-main">
       <ul class="navbar-nav">
         <li class="nav-item">
-        <Link to="/dashboard" class="nav-link">
+        <Link to="/dashboard" class="nav-link" onClick={checkbody}>
             <div class="icon icon-shape icon-sm border-radius-md text-center me-2 d-flex align-items-center justify-content-center">
               <i class="ni ni-tv-2 text-primary text-sm opacity-10"></i>
             </div>
@@ -27,7 +38,7 @@ function Navbar() {
         </li>
         <li class="nav-item">
             {/* เพิ่ม Link เพื่อลิ้งก์ไปยังหน้า table_user.js */}
-            <Link to="/table_user" class="nav-link">
+            <Link to="/table_user" class="nav-link" onClick={checkbody}>
               <div class="icon icon-shape icon-sm border-radius-md text-center me-2 d-flex align-items-center justify-content-center">
                 <i class="ni ni-calendar-grid-58 text-warning text-sm opacity-10"></i>
               </div>
