91b27aceba372074ddcd4063786606dfdc066f0b
jaytanet02 <jaytanet02@gmail.com>
2023-07-31 08:02:13 +0700

    asd

--- src/profile.js
+++ src/profile.js
@@ -19,14 +19,14 @@ function profile() {
                                     <div class="card-body px-0 pt-0 pb-2">
                                         <div class="table-responsive p-0">
                                             <table class="table align-items-center mb-2" >
-                                                
+
                                                 <thead>
                                                     <tr>
                                                         <th class="text-center text-uppercase text-secondary text-x font-weight-bolder opacity-7">PROFILE</th>
                                                         <th class="text-center text-uppercase text-secondary text-x font-weight-bolder opacity-7">Description</th>
                                                         <th class="text-center text-uppercase text-secondary text-x font-weight-bolder opacity-7">Interested in the position</th>
 
-                                                        <th colspan="3"></th>
+                                                        <th colspan="5"></th>
 
 
 
@@ -53,63 +53,57 @@ function profile() {
                                                             </div>
                                                         </td>
 
-                                                        <td style={{ width: '80px' }}>
-                                                            <div class="d-flex flex-column justify-content-center">
+                                                        <td style={{ width: '50px' }}>
+                                                            <center>
+
                                                                 <h6 class="mx-4 ml-3 text-x">
                                                                     backend developer</h6>
-                                                            </div>
+
+                                                            </center>
                                                         </td>
-                                                        <td colspan="3"></td>
-                                                        
+                                                        <td colspan="5"></td>
+
                                                     </tr>
 
-
-               
-                                        
-                                              
                                                     <tr>
                                                         <th class="text-center text-uppercase text-secondary text-x font-weight-bolder opacity-7">Tel</th>
                                                         <th class="text-center text-uppercase text-secondary text-x font-weight-bolder opacity-7">E-mail</th>
                                                         <th class="text-center text-uppercase text-secondary text-x font-weight-bolder opacity-7">Adress</th>
 
-                                                        <th colspan="3"></th>
+                                                        <th colspan="5"></th>
 
 
                                                     </tr>
-                                       
-                                             
+
+
                                                     <tr>
                                                         <td style={{ width: '80px' }}>
-                                                            <div class="d-flex px-2 py-1">
-                                                                <div>
-                                                                    <h6 class="mx-4 ml-3 text-x">
-                                                                        <img src="../assets/img/c7.jpg" class="avatar custom-lg-size mx-3" alt="user1" />
-                                                                        0982846992</h6>
-                                                                </div>
-                                                            </div>
+
+                                                            <center><h6 class="mx-4 ml-3 text-x">
+                                                                <img src="../assets/img/c7.jpg" class="avatar custom-lg-size mx-3" alt="user1" />
+                                                                0982846992</h6></center>
+
                                                         </td>
                                                         <td style={{ width: '80px' }}>
-                                                            <div class="d-flex px-2 py-1">
-                                                                <div>
-                                                                    <h6 class="mx-4 ml-3 text-x">
-                                                                        <img src="../assets/img/c8.png" class="avatar custom-lg-size mx-3" alt="user1" />
-                                                                        jaytanet@gmail.com</h6>
-                                                                </div>
-                                                            </div>
+
+                                                            <center><h6 class="mx-4 ml-3 text-x">
+                                                                <img src="../assets/img/c8.png" class="avatar custom-lg-size mx-3" alt="user1" />
+                                                                jaytanet@gmail.com</h6></center>
+
                                                         </td>
-                                                        <td style={{ width: '80px' }}>
-                                                            <div class="d-flex px-2 py-1">
-                                                                <div>
-                                                                    <h6 class="mx-4 ml-3 text-x">
-                                                                        <img src="../assets/img/c9.jpg" class="avatar custom-lg-size mx-3" alt="user1" />
-                                                                        56 road rama2 alley 51
-                                                                        Subdistrictthakam
-                                                                        District Bang Khun Thian
-                                                                        Bangkok 10150</h6>
-                                                                </div>
-                                                            </div>
+                                                        <td style={{ width: '50px' }}>
+
+                                                            <h6 class="mx-4 ml-3 text-x">
+                                                                <img src="../assets/img/c9.jpg" class="avatar custom-lg-size mx-3" alt="user1" />
+                                                                56 road rama2 alley 51
+                                                                Subdistrictthakam<br />
+                                                                &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                                                                &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                                                                District Bang Khun Thian
+                                                                Bangkok 10150</h6>
+
                                                         </td>
-                                                        <td></td>
+                                                        <td colspan="5"></td>
                                                     </tr>
                                                 </tbody>
                                             </table>
